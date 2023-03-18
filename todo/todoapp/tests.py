ebf37e24d0f633fcb5d624054fa50eddad85b435
dilbazi <you@example.com>
2023-03-18 22:06:42 +0400

    imports

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import json
+from rest_framework import status
+from rest_framework.test import APIRequestFactory,force_authenticate,APIClient,\
+    APITestCase,APISimpleTestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import ProjectModelViewSet
+from .models import Project,TODO
 
-# Create your tests here.
+
