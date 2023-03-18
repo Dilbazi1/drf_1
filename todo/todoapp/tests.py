855b321ce2c35902aeb2eb14514e8b6587f52515
dilbazi <you@example.com>
2023-03-18 22:53:38 +0400

    test2 APIRequestFactory

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -1,22 +1,31 @@
 from django.test import TestCase
 import json
 from rest_framework import status
-from rest_framework.test import APIRequestFactory,force_authenticate,APIClient,\
-    APITestCase,APISimpleTestCase
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, \
+    APITestCase, APISimpleTestCase
 from mixer.backend.django import mixer
 from django.contrib.auth.models import User
 from .views import ProjectModelViewSet
-from .models import Project,TODO
-
+from users.views import UserCustomViewSet
+from .models import Project, TODO
+from users.models import User
 
 
 class TestProjectViewset(TestCase):
     def test_get_list(self):
-        factory=APIRequestFactory()
-        request=factory.get('/api/projects/')
-        view= ProjectModelViewSet.as_view({'get':'list'})
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post('api/users/',
+        {
+            'name':'dilbazi'
+        }, format='json')
+        view=ProjectModelViewSet.as_view({'post':'create'})
         response=view(request)
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
-
-
-
+        print(response.status_code)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
