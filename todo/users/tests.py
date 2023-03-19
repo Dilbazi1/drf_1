6048cd0b1a2d1218854a54bf5d8f2e04faef12e6
dilbazi <you@example.com>
2023-03-19 22:23:40 +0400

    mixer'

--- todo/users/tests.py
+++ todo/users/tests.py
@@ -28,4 +28,54 @@ class TestProjectViewset(TestCase):
         client=APIClient()
         response=client.get(f'/api/users/{user.id}/')
         self.assertEqual(response.status_code,status.HTTP_200_OK)
+    def test_get_detail(self):
+
+        user = User.objects.create(first_name='dilbazi', last_name='mard', email='d@admin.com', username='dilba')
+        client=APIClient()
+        response=client.put(f'/api/users/{user.id}/',{'firstname':'ib','email':"email@gmsil.com"})
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+    def test_edit_admin(self):
+         user=User.objects.create(first_name='dilbazi2', last_name='mard2', email='d2@admin.com', username='dilba2')
+         client=APIClient()
+         admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                               'admin123456')
+         client.login(username='admin',password='admin123456')
+         response = client.put(f'/api/users/{user.id}/', {'first_name': 'ib2', 'last_name':'mard2','username':'ib2','email': "email2@gmsil.com"})
+         self.assertEqual(response.status_code,status.HTTP_200_OK)
+         print(response.data)
+         user = User.objects.get(id=user.id)
+         self.assertEqual(user.first_name, 'ib2')
+         self.assertEqual(user.email,'email2@gmsil.com')
+         client.logout()
+class TestMath(APISimpleTestCase):
+    def test_sqrt(self):
+        import math
+        self.assertEqual(math.sqrt(4),2)
+class TestUserViewset(APITestCase):
+    def test_get_list(self):
+        response=self.client.get('/api/users/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+    def test_get_list(self):
+        response=self.client.get('/api/project/')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+    def test_edit_admin(self):
+        user = User.objects.create(first_name='dilbazi2', last_name='mard2', email='d2@admin.com', username='dilba2')
+
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response=self.client.put(f'/api/users/{user.id}/',{'first_name': 'ib3', 'last_name':'mard3','username':'ib3','email': "email3@gmsil.com"
+
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_edit_mixer(self):
+        user=mixer.blend(User)
+        admin = User.objects.create_superuser('admin', 'admin@admin.com',
+                                              'admin123456')
+        self.client.login(username='admin', password='admin123456')
+        response=self.client.put(f'/api/users/{user.id}/',{'first_name': 'ib3', 'last_name':'mard3','username':'ib3','email': "email3@gmsil.com"
+
+        })
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
 
