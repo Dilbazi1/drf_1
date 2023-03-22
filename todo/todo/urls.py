2724749217505afb146c0cb9e451d3429b79c027
dilbazi <you@example.com>
2023-03-22 17:01:06 +0400

    lesson9

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -33,6 +33,7 @@ from rest_framework.authtoken import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.views.generic import TemplateView
 
 schema_view=get_schema_view(
     openapi.Info(
@@ -60,4 +61,5 @@ urlpatterns = [
     name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
     name='schema-redoc'),
+
 ]
