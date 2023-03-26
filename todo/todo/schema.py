608a6602078f4bb46e7dbd6de6c996b7e176862e
dilbazi <you@example.com>
2023-03-26 20:46:36 +0400

    Запрос к нескольким спискам

--- todo/todo/schema.py
+++ todo/todo/schema.py
@@ -4,30 +4,38 @@ from users.models import User
 from todoapp.models import TODO, Project
 
 
-
-
-
 class ProjectType(DjangoObjectType):
     class Meta:
         model = Project
         fields = '__all__'
+
+
 class UserType(DjangoObjectType):
     class Meta:
         model = User
         fields = '__all__'
+
 
 # вывод данных User
 # class Query(graphene.ObjectType):
 #     # hello = graphene.String(default_value='hi')
 #     all_users = graphene.List(UserType)
 #
-#     def resolve_all_users (root, info):
+#     def resolve_all_users(root, info):
 #         return User.objects.all()
+
+
 class Query(graphene.ObjectType):
     all_projects = graphene.List(ProjectType)
+    all_users = graphene.List(UserType)
+
+
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
+
+    def resolve_all_users(root, info):
+        return User.objects.all()
 
 
 schema = graphene.Schema(query=Query)
