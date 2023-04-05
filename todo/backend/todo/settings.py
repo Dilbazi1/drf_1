b5ff7589f2cd2af1be17f6ead8d30c3ae1c65c27
dilbazi <you@example.com>
2023-04-05 15:57:28 +0400

    docker-compose frontend-f

--- todo/backend/todo/settings.py
+++ todo/backend/todo/settings.py
@@ -27,8 +27,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-]
+    'http://127.0.0.1/',
 
+]
+CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = 'users.User'
 
 # Application definition
